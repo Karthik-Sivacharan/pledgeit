c761b54d372dce29e5bac33d7ef157439ddf093c
Karthik-Sivacharan <karthicksivacharan@gmail.com>
2020-05-24 18:19:18 +0530

    React Avatar Added

--- src/components/Profile/ProfilePicture.js
+++ src/components/Profile/ProfilePicture.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Avatar from 'react-avatar';
 
 class ProfilePicture extends React.Component {
   render() {
@@ -6,14 +7,14 @@ class ProfilePicture extends React.Component {
       <div className="uk-card-default rounded text-center p-4">
         <div className="uk-position-relative my-4">
           <div className="user-profile-photo  m-auto">
-            <img src="images/home-profile.jpg" alt="true" />
+            {/* <img src="images/home-profile.jpg" alt="true" /> */}
+            <Avatar name="Elie Daniels" size="150" textSizeRatio={1.75} textMarginRatio={0.2} round="100px" />
           </div>
           <h4 className="mb-2 mt-3"> Elie Daniels </h4>
-          <div className="uk-position-center">
+          <div className="uk-position-center" style={{marginTop: "15px"}}>
             <div uk-form-custom="true" className="uk-form-custom">
               <input type="file" />
-              <span className="uk-link icon-feather-camera icon-medium text-white">
-                {" "}
+              <span className="uk-link icon-feather-camera icon-semi-medium text-white">
               </span>
             </div>
           </div>
