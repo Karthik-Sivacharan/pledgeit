9d21a0d5fc8667ee082dd6c3be09c166d6fcbcaf
Meivenkatkumar <ln.meivenkatkumar21@gmail.com>
2020-05-25 14:33:59 +0530

    [profile]: updating right dashboard

--- src/components/Profile/ProfileCreate.js
+++ src/components/Profile/ProfileCreate.js
@@ -1,19 +1,114 @@
 import React from "react";
 import ProfileLeft from "./ProfileLeft";
 import ProfileRight from "./ProfileRight";
+import axios from "axios";
 import ProfileCreateFooter from "./ProfileCreateFooter";
+import {config} from "../../config/config";
 
 class ProfileCreate extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      workOn: "",
+      workCategory: "",
+      hashTag: "",
+      handleId: "",
+      userURL: "",
+      token: localStorage.getItem("token")
+    }
+  }
+
+  componentDidMount = async() => {
+    console.log("SAFE REDIRECTS LATER")
+    try{
+      var url = config.APIurl + "/profile/selfDetails/" ;
+      var res = await axios.get(url,{ 
+      headers:{
+          Authorization: this.state.token,
+          'Content-Type': 'application/json'
+      }
+      });
+      if(res.data.success){
+        var profile = res.data.profile;
+        var newState = {
+          workOn: profile.workOn,
+          workCategory: profile.workCategory,
+          hashTag: profile.hashTag,
+          handleId: profile.handleId,
+          userURL: profile.userURL
+        }
+        this.changeState(newState);
+      }
+      else{
+        console.log("Invalid user Credentails", res.data.err);
+      }
+     }
+     catch(err){
+       console.log("error:", err);
+     }
+  }
+
+  changeState = (newState) => {
+    this.setState(newState);
+    this.forceUpdate();
+  }
+
+  save = async () => {
+    var url = config.APIurl + "/profile/updateProfile";
+    this.updateProfile(url);  
+  }
+   
+  publish = async () => {
+    var url = config.APIurl + "/profile/updateProfile";
+    this.updateProfile(url);
+    // and then again publish separately
+  } 
+
+  updateProfile = async (url) => {
+    var profile = {...this.state};
+    delete profile["token"];
+    delete profile["handleId"];
+    if(profile){
+       try{
+        var res = await axios.post(url, profile, {
+          headers:{
+              Authorization: this.state.token,
+              'Content-Type': 'application/json'
+        }});
+        if(res.data.success){
+          console.log("Successfully Update your profile")
+        }
+        else{
+          console.log("Invalid Details",res);
+        }
+       }
+       catch(err){
+         console.log("error:", err);
+       }
+    }
+    else{
+      console.log("error: Invalid Profile Details")
+    }
+  }
+
   render() {
     return (
       <div className="page-content-inner">
         <h2>Setup your page</h2>
         <div uk-grid="true" className="uk-grid uk-grid-stack">
           <ProfileLeft></ProfileLeft>
-          <ProfileRight></ProfileRight>
+          <ProfileRight
+              workOn = {this.state.workOn}
+              workCategory = {this.state.workCategory}
+              hashTag = {this.state.hashTag}
+              userURL = {this.state.userURL}
+              changeState = {this.changeState} 
+          />
         </div>
         <div uk-grid="true" className="uk-grid uk-grid-stack">
-        <button className="btn btn-default grey m-auto mb-3">Publish your page</button>
+        <button id="publish" onClick={this.publish} className="btn btn-default grey m-auto mb-3">Publish your page</button>
+        <button id="save" onClick={this.save} className="btn btn-default grey m-auto mb-3">Save your page</button>
         </div>
         <ProfileCreateFooter></ProfileCreateFooter>
       </div>
--- src/components/Profile/ProfileRight.js
+++ src/components/Profile/ProfileRight.js
@@ -1,13 +1,116 @@
 import React from "react";
-import ProfileAbout from "./ProfileAbout";
-import ProfileStory from "./ProfileStory";
+import Editor from "../Editor/Quill.jsx"
+import $ from "jquery";
 
 class ProfileRight extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      load: 1
+    }
+  }
+
+  setNewStates = () => {
+    var workOn = $("#workOn").val();
+    var workCategory = $("#workCategory").val();
+    var hashTag = $("#hashTag").val();
+    var userURL = $("#userURL").val();
+    var newState = {
+      workOn: workOn,
+      workCategory: workCategory,
+      hashTag: hashTag,
+      userURL: userURL
+    }
+    this.props.changeState(newState);
+  }
+
+  componentWillReceiveProps = () => {
+      var userURL = document.getElementById("userURL").value; 
+      var workOn = document.getElementById("workOn").value; 
+      var workCategory = document.getElementById("workCategory").value; 
+      var hashTag = document.getElementById("hashTag").value;
+      document.getElementById("userURL").value = userURL !="" ? userURL : this.props.userURL;
+      document.getElementById("workOn").value = workOn != "" ? workOn : this.props.workOn;
+      document.getElementById("workCategory").value = workCategory != "" ? workCategory : this.props.workCategory;
+      document.getElementById("hashTag").value = hashTag !="" ? hashTag : this.props.hashTag;
+  } 
+
   render() {
     return (
       <div className="uk-width-expand@m uk-grid-margin uk-first-column">
-        <ProfileAbout></ProfileAbout>
-        <ProfileStory></ProfileStory>
+         <div className="uk-card-default rounded">
+        <div className="uk-flex uk-flex-between uk-flex-middle py-3 px-4">
+          <h5 className="mb-0"> About you</h5>
+          {}
+        </div>
+        <hr className="m-0" />
+        <div
+          className="uk-child-width-1-1@s uk-grid-small p-4 uk-grid uk-grid-stack"
+          uk-grid="true"
+        >
+          <div className="uk-first-column">
+            <h5 className="uk-text-bold mb-2"> What are you working on? </h5>
+            <input
+              type="text"
+              className="uk-input"
+              id="workOn"
+              placeholder="I'm working to save groundwater"
+              onChange={this.setNewStates}
+            />
+          </div>
+          <div className="uk-grid-margin uk-first-column">
+            <h5 className="uk-text-bold mb-2"> Choose your work category? </h5>
+            <select className="uk-select" id="workCategory" onChange={this.setNewStates}>
+              {" "}
+              <option>Heath Worker</option> <option>Social Activist</option>{" "}
+              <option>Environmentalist</option>
+              <option>Other</option>
+            </select>
+          </div>
+          <div className="uk-grid-margin uk-first-column">
+            <h5 className="uk-text-bold mb-2"> Your Hashtag </h5>
+            <input type="text" className="uk-input" id="hashTag" placeholder="#SaveWater" id="hashTag" onChange={this.setNewStates}/>
+          </div>
+          <div className="uk-grid-margin uk-first-column">
+            <h5 className="uk-text-bold mb-2"> Your Website </h5>
+            <input
+              type="text"
+              className="uk-input"
+              placeholder="www.pledge.com"
+              id="userURL"
+              onChange={this.setNewStates}
+            />
+          </div>
+          {}
+        </div>
+      </div>
+      {/* STORY */}
+      <div className="uk-card-default rounded mt-4">
+        <div className="uk-flex uk-flex-between uk-flex-middle py-3 px-4">
+          <h5 className="mb-0"> What's your story?</h5>
+        </div>
+        <div className="uk-flex py-2 px-4">
+
+          <div className="uk-flex uk-flex-middle px-4" uk-margin="true" style={{margin: "0 auto"}}>
+          
+              <button type="button" className="btn btn-icon-label uk-margin-small-top uk-first-column mr-1 ml-1">
+                <span className="btn-inner--icon">
+                  <i className="icon-feather-image" />
+                </span>
+                <span className="btn-inner--text">Upload Image</span>
+              </button>
+
+              <button type="button" className="btn btn-icon-label uk-margin-small-top uk-first-column mr-1 ml-1">
+                <span className="btn-inner--icon">
+                  <i className="icon-feather-video" />
+                </span>
+                <span className="btn-inner--text">Upload Video</span>
+              </button>
+
+          </div>
+        </div>
+        <Editor/>
+      </div>
       </div>
     );
   }
